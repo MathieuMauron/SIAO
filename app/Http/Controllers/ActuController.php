666f90fbd771e317184826bc032ab15f16ae319a
Bigyoan83 <163867092+Bigyoan83@users.noreply.github.com>
2025-03-04 12:28:52 +0100

    maaj

--- app/Http/Controllers/ActuController.php
+++ app/Http/Controllers/ActuController.php
@@ -2,10 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Actualites;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class ActuController extends Controller
 {
-    
+    public function addActualite(Request $request) {
+        Actualites::create([
+            "titre" => $request->titre,
+            "content" => $request->content,
+            "accroche" => $request->accroche,
+            "nom" => Auth::user()->name,
+        ]);  
+        return to_route('actualites');  
+    }
 }
--- resources/views/actualites/add_actu.blade.php
+++ resources/views/actualites/add_actu.blade.php
@@ -6,22 +6,24 @@
             <div class="ajout_article">
                 <section class="bienvenue">
                     <h2>Ajouter un nouvel article</h2>
-                    <form action="/add_actu" method="POST">
-                        @csrf
-                        <div>
-                            <label class="label" for="titre">Titre de l'article :</label><br>
-                            <input type="text" class="titre" name="titre" required>
-                        </div>
-                        <div>
-                            <label class="label" for="accroche">Phrase d'accroche</label><br>
-                            <input class="accroche" type="text" name="accroche" rows="8" required></textarea>
-                        </div>
-                        <div>
-                            <label class="label" for="contenu">Contenu de l'article :</label><br>
-                            <textarea class="content" name="content" rows="8" required></textarea>
-                        </div>
-                        <button type="submit" class="bouton">Ajouter l'article</button>
-                    </form>
+                    <center>
+                        <form action="/add_actu" method="POST">
+                            @csrf
+                            <div>
+                                <label class="label" for="titre">Titre de l'article :</label><br>
+                                <input type="text" class="titre" name="titre" required>
+                            </div>
+                            <div>
+                                <label class="label" for="accroche">Phrase d'accroche</label><br>
+                                <input class="accroche" type="text" name="accroche" rows="8" required></textarea>
+                            </div>
+                            <div>
+                                <label class="label" for="contenu">Contenu de l'article :</label><br>
+                                <textarea class="content" name="content" rows="8" required></textarea>
+                            </div>
+                            <button type="submit" class="bouton">Ajouter l'article</button>
+                        </form>
+                    </center>
                 </section>
             </div>
         </div>
