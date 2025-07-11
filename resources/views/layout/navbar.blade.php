5a18a4a6afbeddc0c4b85673fd0a4472efc1f67a
Bigyoan83 <misecur@protonmail.com>
2025-07-11 17:15:33 +0200

    maj

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -5,7 +5,6 @@
         <li><a href="{{route('equipe')}}">Equipe SIAO / Pôle d'activités</a></li>
         <li><a href="{{route('actualites')}}">Actualité</a></li>
         <li><a href="https://basedeconnaissances.sisiao.dihal.gouv.fr/support/utiliser-le-si-siao" target="_blank">Ressources</a></li>
-        <li><a href="#agenda">Agenda</a></li>
 
         @auth
             <li><a href="{{route('profil')}}">Mon Profil {{ Auth::user()->name }}</a></li>
